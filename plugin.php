610613c7b87ad915630e63e5586c34d3651949c6
Kristopher Kelly <kristopherbkelly@gmail.com>
2008-12-23 21:59:08 +0000

    Removes some hooks and files that are no longer necessary since contribution form metadata is handled via its own element set.

--- plugin.php
+++ plugin.php
@@ -26,10 +26,6 @@ define('CONTRIBUTION_PAGE_PATH', 'contribution/');
 add_plugin_hook('define_routes', 'contribution_routes');
 add_plugin_hook('config_form', 'contribution_config_form');
 add_plugin_hook('config', 'contribution_config');
-add_plugin_hook('before_update_item', 'contribution_save_info');
-// temporarily commented out
-//add_plugin_hook('append_to_item_show', 'contribution_show_info');
-//add_plugin_hook('append_to_item_form', 'contribution_edit_info');
 add_plugin_hook('install', 'contribution_install');
 add_plugin_hook('initialize', 'contribution_initialize');
 add_plugin_hook('define_acl', 'contribution_acl');
@@ -49,29 +45,6 @@ function contribution_routes($router)
     $router->addRoute('contributionAdd', new Zend_Controller_Router_Route($bp, array('module' => 'contribution', 'controller'=> 'index', 'action'=>'add')));
     
 	$router->addRoute('contributionLinks', new Zend_Controller_Router_Route($bp . ':action', array('module' => 'contribution', 'controller'=> 'index')));    
-}
-
-function contribution_show_info($item)
-{
-	include 'show.php';
-}
-
-function contribution_edit_info($item)
-{
-	include 'form.php';
-}
-
-//We need a hook to actually save the input from contribution_edit_info()
-
-function contribution_save_info($item)
-{
-	if(isset($_POST['posting_consent'])) {
-		$item->setMetatext('Posting Consent', $_POST['posting_consent']);
-	}
-	
-	if(isset($_POST['submission_consent'])) {
-		$item->setMetatext('Submission Consent', $_POST['submission_consent']);
-	}	
 }
 
 function contribution_install()
@@ -203,11 +176,6 @@ function contribution_link_to_contribute($text, $options = array())
 	echo '<a href="' . uri(array(), 'contributionAdd') . '" ' . _tag_attributes($options) . ">$text</a>";
 }
 
-function contribution_submission_consent($item)
-{
-	return $item->getMetatext('Submission Consent');
-}
-
 function contribution_embed_consent_form() {
 ?>
 	<form action="<?php echo uri(array('action'=>'submit'), 'contributionLinks'); ?>" id="consent" method="post" accept-charset="utf-8">
@@ -235,11 +203,6 @@ function contribution_embed_consent_form() {
 		<input type="submit" class="submitinput" name="submit" value="Submit" />
 	</form>
 <?php
-}
-
-function contribution_submitted_through_contribution_form($item)
-{
-	return ($item->getMetatext('Online Submission') == 'Yes');
 }
 
 function contribution_is_anonymous($item)
