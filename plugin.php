5838d4e003b82eed2f72c783b9b1b327a3db9f6e
Kristopher Kelly <kristopherbkelly@gmail.com>
2009-03-20 21:33:32 +0000

    Adds ContributionUpgrader class to handle plugin migration and installation.
    Adds migration #2 to add and populate the 'Contributor is Creator' element for existing items.  Bumps migration # to 2.
    
    Removes (refactored into ContributionUpgrader): contribution_install(), contribution_upgrade(), contribution_convert_existing_elements().

--- plugin.php
+++ plugin.php
@@ -20,14 +20,14 @@
 
 define('CONTRIBUTION_PLUGIN_VERSION', 0.2);
 // Define this migration constant to help with upgrading the plugin.
-define('CONTRIBUTION_MIGRATION', 1);
+define('CONTRIBUTION_MIGRATION', 2);
 define('CONTRIBUTION_PAGE_PATH', 'contribution/');
 define('CONTRIBUTORS_PER_PAGE', 10);
 
 add_plugin_hook('define_routes', 'contribution_routes');
 add_plugin_hook('config_form', 'contribution_config_form');
 add_plugin_hook('config', 'contribution_config');
-add_plugin_hook('install', 'contribution_install');
+add_plugin_hook('install', array('ContributionUpgrader', 'install'));
 add_plugin_hook('define_acl', 'contribution_acl');
 
 add_filter('public_navigation_main', 'contribution_public_main_nav');
@@ -49,89 +49,15 @@ function contribution_routes($router)
 	$router->addRoute('contributionLinks', new Zend_Controller_Router_Route($bp . ':action', array('module' => 'contribution', 'controller'=> 'index')));    
 }
 
-function contribution_install()
-{	
-	$db = get_db();
-	
-	contribution_build_element_set(true);
-
-	$db->exec("CREATE TABLE IF NOT EXISTS `$db->Contributor` (
-			`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-			`entity_id` BIGINT UNSIGNED NOT NULL ,
-			`birth_year` YEAR NULL,
-			`gender` TINYTEXT NULL,
-			`race` TINYTEXT NULL,
-			`occupation` TINYTEXT NULL,
-			`zipcode` TINYTEXT NULL,
-			`ip_address` TINYTEXT NOT NULL
-			) ENGINE = MYISAM ;");
-		
-	set_option('contribution_plugin_version', CONTRIBUTION_PLUGIN_VERSION);
-	set_option('contribution_page_path', CONTRIBUTION_PAGE_PATH);
-	set_option('contribution_require_tos_and_pp', FALSE);
-	set_option('contribution_db_migration', CONTRIBUTION_MIGRATION);
-}
-
-function contribution_build_element_set($buildElements=true)
-{
-    try {
-        $elementSet = new ElementSet;
-	    $elementSet->name = "Contribution Form";
-	    $elementSet->description = "The set of elements containing metadata from the Contribution form.";
-
-	    if ($buildElements) {
-	        $elementSet->addElements(array(
-	             array(
-	                'name'=>'Online Submission',
-	                'description'=>'Indicates whether or not this Item has been contributed from a front-end contribution form.',
-	                'record_type'=>'Item'),
-	             array(
-	                 'name'=>'Posting Consent',
-	                 'description'=>'Indicates whether or not the contributor of this Item has given permission to post this to the archive. (Yes/No)',
-	                 'record_type'=>'Item'),
-	             array(
-	                 'name'=>'Submission Consent',
-	                 'description'=>'Indicates whether or not the contributor of this Item has given permission to submit this to the archive. (Yes/No)',
-	                 'record_type'=>'Item')));
-	    }
-
-	    // Die if this doesn't save properly.
-	    $elementSet->forceSave();
-    } catch (Exception $e) {
-        var_dump($e);exit;
-    }
-    return $elementSet->id;
-}
-
-function contribution_convert_existing_elements()
-{
-    // Retrieve the existing elements and modify them to belong to the Contribution Form element set.
-    $db = get_db();
-    $sql  = "SELECT id FROM $db->ElementSet WHERE name = 'Additional Item Metadata'";
-    $additionalItemElementSetId = $db->fetchOne($sql);
-
-    // If the Additional Item Metadata element set does not exist for whatever
-    // reason, there is no pre-existing plugin data to convert so just build
-    // the whole thing from scratch.  Otherwise just make the new element set
-    // w/o the new elements and convert the old ones.
-    $newElementSetId = contribution_build_element_set(!$additionalItemElementSetId);
-
-    // Update the existing elements w/o interacting with the ElementSet models.
-    try {
-        $db->query(
-	            "UPDATE $db->Element SET element_set_id = ? 
-	            WHERE element_set_id = ? AND name IN (" . $db->quote(
-	                array('Online Submission', 'Posting Consent', 'Submission Consent')) .
-	            ") LIMIT 3",
-	            array($newElementSetId, $additionalItemElementSetId));
-    } catch (Exception $e) {
-        var_dump($e);exit;
-    }
-}
-
 function contribution_config_form()
 {
-    contribution_upgrade();
+    // Deal with upgrading the plugin if necessary.
+    $pluginVersion = (int)get_option('contribution_db_migration');
+    // Skip the migrations if we don't need it.
+    if ($pluginVersion < CONTRIBUTION_MIGRATION) {
+        ContributionUpgrader::upgrade($pluginVersion, CONTRIBUTION_MIGRATION);
+    }    
+    
     
 	$textInputSize = 30;
 	$textAreaRows = 10;
@@ -254,16 +180,6 @@ function contribution_public_main_nav($navArray) {
     return $navArray;
 }
 
-function contribution_upgrade()
-{
-    $pluginVersion = get_option('contribution_db_migration');
-    if ($pluginVersion < CONTRIBUTION_MIGRATION) {
-        contribution_convert_existing_elements();
-        // Bump up the database's migration #
-        set_option('contribution_db_migration', CONTRIBUTION_MIGRATION);
-    }
-}
-
 function contribution_acl($acl)
 {
     $acl->loadResourceList(array('Contribution_Index'=>array('browse', 'edit', 'delete')));
@@ -272,6 +188,8 @@ function contribution_acl($acl)
 /**
  * A prototype of the insert_item() helper, which will be in the core in 1.0.
  *
+ * FIXME: Remove this and the InsertItemHelper in this plugin.
+ * 
  * @uses InsertItemHelper
  * @param array $itemMetadata 
  * @param array $elementTexts 
@@ -336,3 +254,285 @@ function contribution_show_anonymous_contributor($contributorName, $item)
 
     return $contributorName;
 }
+
+/**
+ * Encapsulates the handling of installation/upgrade of the Contribution plugin.
+ *
+ * @package Contribution
+ * @copyright Center for History and New Media, 2009
+ **/
+class ContributionUpgrader
+{    
+    static protected $_elementsInSet = array(
+         'Online Submission' => array(
+            'name'=>'Online Submission',
+            'description'=>'Indicates whether or not this Item has been contributed from a front-end contribution form.',
+            'record_type'=>'Item'),
+         'Posting Consent' => array(
+             'name'=>'Posting Consent',
+             'description'=>'Indicates whether or not the contributor of this Item has given permission to post this to the archive. (Yes/No)',
+             'record_type'=>'Item'),
+         'Submission Consent' => array(
+             'name'=>'Submission Consent',
+             'description'=>'Indicates whether or not the contributor of this Item has given permission to submit this to the archive. (Yes/No)',
+             'record_type'=>'Item'),
+         'Contributor is Creator' => array(
+             'name'=>'Contributor is Creator',
+             'description'=>'Indicates whether or not the contributor of the Item is responsible for its creation.',
+             'record_type'=>'Item'
+             ));
+    
+    /**
+     * Install the Contribution plugin.
+     * 
+     * Create the 'Contribution Form' element set with 4 elements.  Create the
+     * 'contributors' table in the database.  Set all the appropriate options
+     * to the default values.
+     * 
+     * @return void
+     **/
+    public static function install()
+    {
+    	$elementSet = self::_insertElementSet();
+        self::_addElementsToElementSet($elementSet);
+        self::_addContributorsTable();
+    	
+    	set_option('contribution_plugin_version', CONTRIBUTION_PLUGIN_VERSION);
+    	set_option('contribution_page_path', CONTRIBUTION_PAGE_PATH);
+    	set_option('contribution_require_tos_and_pp', FALSE);
+    	set_option('contribution_db_migration', CONTRIBUTION_MIGRATION);
+    }
+    
+    /**
+     * Upgrade the Contribution plugins from an existing version to another.
+     * 
+     * @param integer
+     * @param integer
+     * @return void
+     **/
+    public static function upgrade($from, $to)
+    {
+        $upgrader = new self;
+        
+        $currentMigration = $from;
+        while ($currentMigration < $to) {
+            $migrateMethod = '_to' . ++$currentMigration;
+            $upgrader->$migrateMethod();
+        }
+
+        set_option('contribution_db_migration', $to);
+    }
+    
+    /**
+     * Upgrade the Contribution plugin database from its initial version.
+     * 
+     * In this case, '1' represents the first database migration and not the 
+     * initial version of the plugin.  Performs the following tasks:
+     * 
+     * 1) Add the 'Contribution Form' element set.
+     * 2) The existing elements would have fallen under the 'Additional Item
+     * Metadata' element set, so if that set exists, then move all those elements
+     * to the 'Contribution Form' set.
+     * 3) Otherwise, add the elements as normal.
+     * 
+     * @return void
+     **/
+    private function _to1()
+    {
+        // Retrieve the existing elements and modify them to belong to the Contribution Form element set.
+        $db = get_db();
+        $sql  = "SELECT id FROM $db->ElementSet WHERE name = 'Additional Item Metadata'";
+        $additionalItemElementSetId = $db->fetchOne($sql);
+
+        // If the Additional Item Metadata element set does not exist for whatever
+        // reason, there is no pre-existing plugin data to convert so just build
+        // the whole thing from scratch.  Otherwise just make the new element set
+        // w/o the new elements and convert the old ones.
+        $contributionFormElementSet = self::_insertElementSet();
+        if (!$additionalItemElementSetId) {
+            // Add the elements from scratch
+            self::_addElementsToElementSet($contributionFormElementSet);
+        } else {
+            // Otherwise, convert the existing elements.
+            // Update the existing elements w/o interacting with the ElementSet models.
+            try {
+                $db->query(
+        	            "UPDATE $db->Element SET element_set_id = ? 
+        	            WHERE element_set_id = ? AND name IN (" . $db->quote(
+        	                array('Online Submission', 'Posting Consent', 'Submission Consent')) .
+        	            ") LIMIT 3",
+        	            array($contributionFormElementSet->id, $additionalItemElementSetId));
+            } catch (Exception $e) {
+                var_dump($e);exit;
+            }
+        }
+    }
+    
+    /**
+     * The second database migration for the Contribution plugin.
+     * 
+     * This will add the 'Contribution is Creator' element and then populate it
+     * with the 'Yes' flag for all the items that were submitted through the 
+     * Contribution plugin that have the same 'Contributor' and 'Creator' field
+     * values.
+     * 
+     * @return void
+     **/
+    private function _to2()
+    {
+        $this->_addContributorIsCreatorElement();
+        $this->_fixItemsWhereContributorIsCreator();
+    }
+    
+    /**
+     * Add the 'Contributor is Creator' element to the Contribution Form 
+     * element set.
+     * 
+     * If this element already exists, do nothing.
+     * 
+     * @return void
+     **/
+    private function _addContributorIsCreatorElement()
+    {
+        // Test to see whether or not we already have this element.
+        $element = get_db()->getTable('Element')->findByElementSetNameAndElementName('Contribution Form', 'Contributor is Creator');
+        if ($element) {
+            return;
+        }
+        
+        // Add the 'Contributor is Creator' element.
+        $elementSet = $this->_getElementSet();
+        $elementSet->addElements(array(self::$_elementsInSet['Contributor is Creator']));
+        $elementSet->forceSave();
+    }
+    
+    /**
+     * Update all the items that were previously submitted through the Contribution
+     * plugin so that they contain the correct 'Contributor is Creator' flag.
+     * 
+     * TODO: Stress test this on large sets.
+     * @return void
+     **/
+    private function _fixItemsWhereContributorIsCreator()
+    {
+        // Migrate existing data for contributions.
+        // If the item is listed as a user contribution (Online Submission = 'Yes')
+        // And Dublin Core Creator = Dublin Core Contributor
+        // Then UPDATE 'Contributor is Creator' = 'Yes'.
+        
+        $elementTable = get_db()->getTable('Element');
+        $onlineSubmissionElementId = $elementTable->findByElementSetNameAndElementName('Contribution Form', 'Online Submission')->id;
+        
+        // var_dump(compact('creatorElementId', 'contributorElementId', 'onlineSubmissionElementId', 'postingConsentElementId'));exit;
+        $criteria = array(array(
+                            'terms'=>'Yes',
+                            'type'=>'contains',
+                            'element_id'=>$onlineSubmissionElementId));
+        
+        $itemSelectSql = get_db()->getTable('Item')->getSelectForFindBy(array('advanced_search'=>$criteria));
+        
+        // SELECT only the record IDs for items that have the same value for 
+        // Dublin Core Contributor and Creator.
+        // 
+        // How we do this: select from the set of all Dublin Core Creators,
+        // inner join that against the set of all Dublin Core Contributors 
+        // (using the record_id), and then a simple equivalence test between
+        // the retrieved creator and contributor values.
+        $itemSelectSql->where("i.id IN (
+        SELECT creator_set.record_id FROM (
+            SELECT etx.id, etx.record_id, etx.text as creator FROM element_texts etx
+            INNER JOIN elements e ON e.id = etx.element_id    
+            INNER JOIN element_sets es ON es.id = e.element_set_id
+            WHERE e.name = 'Creator' AND es.name = 'Dublin Core'    
+        ) creator_set
+        INNER JOIN (
+            SELECT etx.id, etx.record_id, etx.text as contributor
+            FROM element_texts etx
+            INNER JOIN elements e ON e.id = etx.element_id    
+            INNER JOIN element_sets es ON es.id = e.element_set_id
+            WHERE e.name = 'Contributor' AND es.name = 'Dublin Core'
+        ) contributor_set ON contributor_set.record_id = creator_set.record_id
+        WHERE creator = contributor        
+        )"); 
+        
+        // Get the total # of items to change
+                
+        // FIXME: Chunk this data retrieval so it doesn't die when iterating through
+        // potentially thousands of items.
+        
+        $itemsToFix = get_db()->getTable('Item')->fetchObjects($itemSelectSql);
+        
+        foreach ($itemsToFix as $key => $item) {
+            update_item($item, array(), 
+            array('Contribution Form'=>
+                array('Contributor is Creator'=>
+                    array(array('text'=>'Yes', 'html'=>false)))));
+            release_object($item);
+        }
+    }
+     
+    /**
+     * Create the 'contributors' table in the database.
+     *
+     * @return void
+     **/
+    private static function _addContributorsTable()
+    {
+        $db = get_db();
+        $db->exec("CREATE TABLE IF NOT EXISTS `$db->Contributor` (
+    			`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    			`entity_id` BIGINT UNSIGNED NOT NULL ,
+    			`birth_year` YEAR NULL,
+    			`gender` TINYTEXT NULL,
+    			`race` TINYTEXT NULL,
+    			`occupation` TINYTEXT NULL,
+    			`zipcode` TINYTEXT NULL,
+    			`ip_address` TINYTEXT NOT NULL
+    			) ENGINE = MYISAM ;");
+    }
+    
+    /**
+     * Retrieve the 'Contribution Form' element set object.
+     * 
+     * @return ElementSet|null
+     **/
+    private function _getElementSet()
+    {
+        $table = get_db()->getTable('ElementSet');
+        $elementSetSelect = $table->getSelect()
+                         ->where('es.name = ?', 'Contribution Form')
+                         ->limit(1);
+        return $table->fetchObject($elementSetSelect);
+    }
+    
+    /**
+     * Add the 'Contribution Form' element set to the database.
+     * 
+     * @return ElementSet
+     **/
+    private static function _insertElementSet()
+    {
+        $elementSet = new ElementSet;
+        $elementSet->name = "Contribution Form";
+        $elementSet->description = "The set of elements containing metadata from the Contribution form.";
+
+        // Die if this doesn't save properly.
+        $elementSet->forceSave();
+        return $elementSet;        
+    }
+    
+    /**
+     * Adds all the default Contribution Form elements to an element set 
+     * (should be the Contribution Form element set).
+     * 
+     * @param ElementSet $elementSet 
+     * @return void
+     **/
+    private static function _addElementsToElementSet($elementSet)
+    {
+        $elementSet->addElements(self::$_elementsInSet);
+        $elementSet->forceSave();
+    }
+}
+
+
