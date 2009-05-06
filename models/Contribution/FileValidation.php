f16ce47470e750695de41898930c1cdf6181c6f9
Kristopher Kelly <kristopherbkelly@gmail.com>
2009-05-06 18:54:56 +0000

    Fixes Contribution_FileValidation to use the default file ingest validators instead of creating a separate set for the Contribution form.

--- models/Contribution/FileValidation.php
+++ models/Contribution/FileValidation.php
@@ -7,11 +7,8 @@
  **/
 
 /**
- * Assign the correct validators for file ingest based on the type of 
- * Contribution that is being made.
- * 
- * Stores validator info for the various item types and interacts with the system
- * through the file_ingest_validators' filter.
+ * Manipulate the set of file ingest validators that are active for the 
+ * Contribution form.
  *
  * @package Contribution
  * @copyright Center for History and New Media, 2009
@@ -19,48 +16,18 @@
 class Contribution_FileValidation
 {
     const FILTER_NAME = 'file_ingest_validators';
-    
-    /**
-     * @var string Name of the Item Type to which the contributed item belongs.
-     */
-    protected $_itemTypeName;
-    
-    /**
-     * @var array Keyed to the name of the Item Type, contains a list of 
-     * validators to use.
-     */
-    protected $_itemTypeValidators = array(
-        'Still Image' => array(
-            array('name'    => 'MimeType', 
-                  'options' => 'image', // All image media types.
-                  'message' => "The type of file uploaded (%type%) is not recognized as a valid image.  Please change the file format or upload a different image.")),
-        'Moving Image' => array(
-            array('name'    => 'MimeType', 
-                  'options' => 'video',
-                  'message' => 'The type of file uploaded (%type%) is not recognized as a valid movie.  Please change the file format or upload a different movie.')),
-        'Sound' => array(
-            array('name'    => 'MimeType', 
-                  'options' => 'audio',
-                  'message' => 'The type of file uploaded (%type%) is not recognized as a valid audio file.  Please change the format or upload a different file.'))
-    );
-    
+        
     /**
      * TODO: Are there any validators in this set?
      * @var array Set of validators that will always apply to contributed files.
      */
     protected $_defaultValidators = array();
     
-    /**
-     * Constructor.
-     * 
-     * @param string
-     * @return void
-     **/
-    public function __construct($itemTypeName)
-    {
-        $this->_itemTypeName = $itemTypeName;
-    }
-    
+    protected $_validationMessages = array(
+        'extension whitelist' => "The uploaded file (%value%) has a disallowed file extension.  Please modify the file format or upload a different file.",
+        'MIME type whitelist' => "The uploaded file (%value%) has a disallowed media type.  Please modify the file format or upload a different file."
+    );
+        
     /**
      * Instantiate a validator.
      * 
@@ -87,32 +54,29 @@ class Contribution_FileValidation
     }
     
     /**
-     * Filter the set of validators so that it only contains the proper
-     * whitelists for the given Item Type that is being contributed.
+     * Modify the validation error messages for the default file ingest 
+     * validators.
      * 
-     * Dump the existing set in favor of the new set.
+     * Also add additional validators to the stack if necessary.
      * 
      * @param array
      * @return array
      **/
     public function filterValidators($validators)
     {        
-        $validators = array();
         // Add the default validators.
         foreach ($this->_defaultValidators as $validatorInfo) {
             $validators[] = $this->_getValidatorFromInfo($validatorInfo);
         }
         
-        // Add the Item Type validators.
-        $itemTypeValidatorInfo = $this->_itemTypeValidators[$this->_itemTypeName];
-        
-        if (!$itemTypeValidatorInfo) {
-            throw new Contribution_Exception("Invalid Item Type provided!");
+        // Reset the validation error messages to hide some info from public
+        // site visitors.
+        foreach ($this->_validationMessages as $validatorName => $validatorMsg) {
+            if (array_key_exists($validatorName, $validators)) {
+                $validators[$validatorName]->setMessage($validatorMsg);
+            }
         }
         
-        foreach ($itemTypeValidatorInfo as $validatorInfo) {
-            $validators[] = $this->_getValidatorFromInfo($validatorInfo);
-        }
         return $validators;
     }
 }
