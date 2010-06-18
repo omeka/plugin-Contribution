6ad46fb973213e2c73160d7a607ebc884ee6827f
John Flatness <john@zerocrates.org>
2010-06-18 18:39:21 +0000

    Adds view helper class to override Omeka ElementForm helper.

---
+++ helpers/ElementForm.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @version $Id$
+ * @author CHNM
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @copyright Center for History and New Media, 2010
+ * @package Contribution
+ */
+
+/**
+ * Overrides Omeka's ElementForm helper to allow for custom display of fields.
+ */
+class Contribution_View_Helper_ElementForm extends Omeka_View_Helper_ElementForm
+{
+	protected function _getFieldLabel()
+	{
+		
+	}
+}
