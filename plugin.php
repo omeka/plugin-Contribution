541133fae311366062153875ff6342c0a781aee1
John Flatness <john@zerocrates.org>
2010-06-18 18:39:18 +0000

    Coding standards fixes.

--- plugin.php
+++ plugin.php
@@ -1,25 +1,30 @@
 <?php
 /**
-* Contribution plugin.
-* @author CHNM
-* @version $Id$
-* @copyright Center for History and New Media, 2007-2010
-* @package Contribution
-*/
+ * @version $Id$
+ * @author CHNM
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @copyright Center for History and New Media, 2010
+ * @package Contribution
+ */
+
+define('CONTRIBUTION_PLUGIN_DIR', dirname(__FILE__));
+define('CONTRIBUTION_HELPERS_DIR', CONTRIBUTION_PLUGIN_DIR
+                                 . DIRECTORY_SEPARATOR
+                                 . 'helpers');
 
 /**
 * Contribution plugin class
 */
 class Contribution
 {
-    private $db;
+    private $_db;
     
     /**
      * Initializes instance properties and hooks the plugin into Omeka.
      */
     public function __construct()
     {
-        $this->db = get_db();
+        $this->_db = get_db();
         self::addHooksAndFilters();
     }
     
@@ -37,7 +42,7 @@ class Contribution
      */
     public function install()
     {
-        $sql = "CREATE TABLE IF NOT EXISTS `{$this->db->prefix}contribution_types` (
+        $sql = "CREATE TABLE IF NOT EXISTS `{$this->_db->prefix}contribution_types` (
             `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
             `item_type_id` INT UNSIGNED NOT NULL,
             `alias` VARCHAR(255) NOT NULL,
@@ -46,32 +51,34 @@ class Contribution
             PRIMARY KEY (`id`),
             UNIQUE KEY `item_type_id` (`item_type_id`)
             ) ENGINE=MyISAM;";
-        $this->db->query($sql);
+        $this->_db->query($sql);
         
-        $sql = "CREATE TABLE IF NOT EXISTS `{$this->db->prefix}contribution_type_elements` (
+        $sql = "CREATE TABLE IF NOT EXISTS `{$this->_db->prefix}contribution_type_elements` (
             `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
             `type_id` INT UNSIGNED NOT NULL,
             `alias` VARCHAR(255) NOT NULL,
             PRIMARY KEY (`id`),
             UNIQUE KEY `type_id` (`type_id`)
             ) ENGINE=MyISAM;";
-        $this->db->query($sql);
+        $this->_db->query($sql);
 
-        $sql = "CREATE TABLE IF NOT EXISTS `{$this->db->prefix}contribution_contributors` (
+        $sql = "CREATE TABLE IF NOT EXISTS `{$this->_db->prefix}contribution_contributors` (
             `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+			`name` VARCHAR(255) NOT NULL,
             `email` VARCHAR(255) NOT NULL,
+			`ip_address` INT UNSIGNED NOT NULL,
             PRIMARY KEY (`id`),
             UNIQUE KEY `email` (`email`)
             ) ENGINE=MyISAM;";
-        $this->db->query($sql);
+        $this->_db->query($sql);
 
-        $sql = "CREATE TABLE IF NOT EXISTS `{$this->db->prefix}contribution_contributed_items` (
+        $sql = "CREATE TABLE IF NOT EXISTS `{$this->_db->prefix}contribution_contributed_items` (
             `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
             `item_id` INT UNSIGNED NOT NULL,
             PRIMARY KEY (`id`),
             UNIQUE KEY `item_id` (`item_id`)
             ) ENGINE=MyISAM;";
-        $this->db->query($sql);
+        $this->_db->query($sql);
 
         $this->_createContributorElementSet();
     }
@@ -79,18 +86,25 @@ class Contribution
     public function uninstall()
     {
         $sql = "DROP TABLE IF EXISTS
-            {$this->db->prefix}contribution_types,
-            {$this->db->prefix}contriubtion_type_elements,
-            {$this->db->prefix}contribution_contributors,
-            {$this->db->prefix}contribution_contributed_items;";
-        $this->db->query($sql);
+            {$this->_db->prefix}contribution_types,
+            {$this->_db->prefix}contriubtion_type_elements,
+            {$this->_db->prefix}contribution_contributors,
+            {$this->_db->prefix}contribution_contributed_items;";
+        $this->_db->query($sql);
 
-        $recordTypeTable = $this->db->getTable('RecordType');
+        $recordTypeTable = $this->_db->getTable('RecordType');
         $recordTypeId = $recordTypeTable->findIdFromName('ContributionContributor');
         if ($recordTypeId !== null) {
             $recordType = $recordTypeTable->find($recordTypeId);
             $recordType->delete();
         }
+
+		$elementSetTable = $this->_db->getTable('ElementSet');
+		$elementSetId = $elementSetTable->findIdFromName('Contributor Information');
+		if ($elementSetId !== null) {
+			$elementSet = $elementSetTable->find($elementSetId);
+			$elementSet->delete();
+		}
         
         // For now, leave the element set there
         // Probably will make sense to delete it, since the contributors it
