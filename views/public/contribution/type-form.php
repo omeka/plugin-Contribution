a68df89cf1189ee04bb813014b08fa949654c089
John Flatness <john@zerocrates.org>
2010-07-06 19:09:15 +0000

    Moves file upload to bottom of form when optional.

--- views/public/contribution/type-form.php
+++ views/public/contribution/type-form.php
@@ -1,7 +1,7 @@
 <?php if (!$type): ?>
 <p>You must choose a contribution type to continue.</p>
 <?php else: ?>
-<?php if ($type->file_allowed): ?>
+<?php if ($type->file_allowed && $type->file_required): ?>
 <div class="field">
 	<input type="hidden" name="MAX_FILE_SIZE" value="30000000" />
 	<label for="file">Upload a file</label>
@@ -11,4 +11,11 @@
 <?php foreach ($type->getTypeElements() as $element) {
     echo $this->elementForm($element, $item);
 } ?>
+<?php if ($type->file_allowed && !$type->file_required): ?>
+<div class="field">
+	<input type="hidden" name="MAX_FILE_SIZE" value="30000000" />
+	<label for="file">Upload a file <br /> (Optional)</label>
+	<input type="file" class="textinput" name="contributed_file" id="file-upload" class="fileinput" />	
+</div>
+<?php endif; ?>
 <?php endif; ?>
