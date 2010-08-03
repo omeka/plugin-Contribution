11a0bf8915cc9392ddb1e32f5bfaaa52bc6724b5
John Flatness <john@zerocrates.org>
2010-08-03 15:19:42 +0000

    Updates Contribution tests to use suite-style construction.

--- tests/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php 
-define('CONTRIBUTION_PLUGIN_DIR', dirname(dirname(__FILE__)));
-$baseBootstrap = dirname(dirname(dirname(dirname(__FILE__)))) . '/application/tests/bootstrap.php';
-require_once $baseBootstrap;
