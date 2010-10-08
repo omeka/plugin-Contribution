8f743ba7feae2b054db044930b7f93327fa653e7
John Flatness <john@zerocrates.org>
2010-10-08 18:57:39 +0000

    Documents public form JS.

--- views/public/javascripts/contribution-public-form.js
+++ views/public/javascripts/contribution-public-form.js
@@ -1,30 +1,37 @@
 function enableContributionAjaxForm(url) {
     jQuery(document).ready(function() {
+        // Div that will contain the AJAX'ed form.
         var form = jQuery('#contribution-type-form');
+        // Select element that controls the AJAX form.
         var contributionType = jQuery('#contribution-type');
-        var submit = jQuery('#contribution-confirm-submit');
-        var contributorMetadata = jQuery('#contribution-contributor-metadata');
+        // Elements that should be hidden when there is no type form on the page.
+        var elementsToHide = jQuery('#contribution-confirm-submit, #contribution-contributor-metadata');
+        // Duration of hide/show animation.
+        var duration = 400;
 
+        // Remove the noscript-fallback type submit button.
         jQuery('#submit-type').remove();
-        
+
+        // When the select is changed, AJAX in the type form
         contributionType.change(function () {
             var value = this.value;
-            submit.hide();
-            contributorMetadata.hide();
-            form.hide(400, function() {
+            elementsToHide.hide();
+            form.hide(duration, function() {
                 form.empty();
                 if (value != "") {
                     jQuery.post(url, {contribution_type: value}, function(data) {
                        form.append(data); 
-                       form.show(400, function() {
+                       form.show(duration, function() {
                            form.trigger('contribution-form-shown');
-                           submit.show();
-                           contributorMetadata.show();
+                           elementsToHide.show();
                        });
                     });
                 }
             });
         });
+
+        // Do some quick-and-dirty validation of some of the required inputs.
+        // TODO: replace alerts with a better notification method.
         jQuery('#form-submit').click(function (event) {
             var name = jQuery('#contributor-name').val();
             var email = jQuery('#contributor-email').val();
