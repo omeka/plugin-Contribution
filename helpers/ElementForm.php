d9d731442d34379b918ef683b927ce7cd9728051
John Flatness <john@zerocrates.org>
2012-03-13 11:28:39 -0400

    Fix ElementForm include for Omeka 2.0.

--- helpers/ElementForm.php
+++ helpers/ElementForm.php
@@ -6,7 +6,11 @@
  * @package Contribution
  */
 
-require_once HELPER_DIR . DIRECTORY_SEPARATOR . 'ElementForm.php';
+if (version_compare(OMEKA_VERSION, '2.0-dev', '<')) {
+    require_once HELPER_DIR . DIRECTORY_SEPARATOR . 'ElementForm.php';
+} else {
+    require_once VIEW_HELPERS_DIR . DIRECTORY_SEPARATOR . 'ElementForm.php';
+}
 
 /**
  * Overrides Omeka's ElementForm helper to allow for custom display of fields.
