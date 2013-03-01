7a956fb04805217de00b256a6082f07dde0709b3
patrickmj <patrickmjchnm@gmail.com>
2013-03-01 13:00:27 -0500

    minor cleanup

--- ContributionPlugin.php
+++ ContributionPlugin.php
@@ -212,37 +212,33 @@ class ContributionPlugin extends Omeka_Plugin_AbstractPlugin
         // Only apply custom routes on public theme.
         // The wildcards on both routes make these routes always apply for the
         // contribution controller.
+
         // get the base path
-
-            // get the base path
-            $bp = get_option('contribution_page_path');
-
-            if ($bp) {
-                $router->addRoute('contributionCustom',
-                    new Zend_Controller_Router_Route("{$bp}/:action/*",
-                        array('module'     => 'contribution',
-                              'controller' => 'contribution',
-                              'action'     => 'contribute')));
-            }else{
-            
-        $router->addRoute('contributionDefault',
-              new Zend_Controller_Router_Route('contribution/:action/*',
+        $bp = get_option('contribution_page_path');
+        if ($bp) {
+            $router->addRoute('contributionCustom',
+                new Zend_Controller_Router_Route("$bp/:action/*",
                     array('module'     => 'contribution',
                           'controller' => 'contribution',
                           'action'     => 'contribute')));
-            
-            }
+        } else {
+                
+            $router->addRoute('contributionDefault',
+                  new Zend_Controller_Router_Route('contribution/:action/*',
+                        array('module'     => 'contribution',
+                              'controller' => 'contribution',
+                              'action'     => 'contribute')));
+                
+        }
       
-         if(is_admin_theme()){
+        if(is_admin_theme()){
             $router->addRoute('contributionAdmin',
                 new Zend_Controller_Router_Route('contribution/:controller/:action/*',
                     array('module' => 'contribution',
                           'controller' => 'index',
                           'action' => 'index')));
-        }
+        }    
     }
-
-
 
     /**
      * Append a Contribution entry to the admin navigation.
