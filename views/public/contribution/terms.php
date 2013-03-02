8c8c1e85c90ed6853803eb207fbb6a122558c140
patrickmj <patrickmjchnm@gmail.com>
2013-03-02 15:00:06 -0500

    Make the terms of service page less lame

--- views/public/contribution/terms.php
+++ views/public/contribution/terms.php
@@ -1,1 +1,10 @@
+<?php 
+$head = array('title' => __('Contribution Terms of Service'));
+echo head($head);
+?>
+
+<div id="primary">
+<h1><?php echo $head['title']; ?></h1>
 <?php echo get_option('contribution_consent_text'); ?>
+</div>
+<?php echo foot(); ?>
