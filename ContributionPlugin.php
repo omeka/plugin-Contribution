d812bddeb2c8d317e5faf522a633c720241c8450
patrickmj <patrickmjchnm@gmail.com>
2014-11-18 11:38:53 -0500

    more upgrade fixes

--- ContributionPlugin.php
+++ ContributionPlugin.php
@@ -200,7 +200,11 @@ class ContributionPlugin extends Omeka_Plugin_AbstractPlugin
             Zend_Registry::get('bootstrap')->getResource('jobs')->sendLongRunning('ContributionImportUsers');
             //if the optional UserProfiles plugin is installed, handle the upgrade via the configuration page
             $sql = "ALTER TABLE `{$this->_db->ContributionTypeElement}` ADD `long_text` BOOLEAN DEFAULT TRUE";
-            $this->_db->query($sql);
+            try {
+                $this->_db->query($sql);
+            } catch(Exception $e) {
+                _log($e);
+            }
             $contributionTypeElements = $this->_db->getTable('ContributionTypeElement')->findAll();
             foreach($contributionTypeElements as $typeElement) {
                 $typeElement->long_text = true;
@@ -210,8 +214,26 @@ class ContributionPlugin extends Omeka_Plugin_AbstractPlugin
             //clean up contributed item records if the corresponding item has been deleted
             //earlier verison of the plugin did not use the delete hook
             $sql = "DELETE  FROM `{$this->_db->ContributionContributedItem}` WHERE NOT EXISTS (SELECT 1 FROM `{$this->_db->prefix}items`  WHERE `{$this->_db->prefix}contribution_contributed_items`.`item_id` = `{$this->_db->prefix}items`.`id`)";
-           
-            $this->_db->query($sql);   
+
+            $this->_db->query($sql);
+        }
+        if (version_compare($oldVersion, '3.0.2', '<')) {
+            $sql = "
+                ALTER TABLE `{$this->_db->ContributionContributedItem}` CHANGE `contributor_posting` `anonymous` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '0';
+                ";
+            try {
+                $this->_db->query($sql);
+            } catch(Exception $e) {
+                _log($e);
+            }
+            $sql = "
+                ALTER TABLE `{$this->_db->ContributionContributedItem}` DROP `contributor_id` ;
+            ";
+            try {
+                $this->_db->query($sql);
+            } catch(Exception $e) {
+                _log($e);
+            }
         }
     }
 
