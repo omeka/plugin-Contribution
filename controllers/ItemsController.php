fb1ba2e98c10e42f06e37d85379a7a9fa6ba625f
patrickmj <patrickmjchnm@gmail.com>
2015-02-22 12:39:03 -0500

    even better than the Strict standards fix, use the default

--- controllers/ItemsController.php
+++ controllers/ItemsController.php
@@ -2,15 +2,6 @@
 
 class Contribution_ItemsController extends Omeka_Controller_AbstractActionController
 {
-    public function _getBrowseRecordsPerPage($pluralName = null)
-    {
-        if (is_admin_theme()) {
-            return (int) get_option('per_page_admin');
-        } else {
-            return (int) get_option('per_page_public');
-        }
-    }
-
     public function init()
     {
         $this->_helper->db->setDefaultModelName('ContributionContributedItem');
