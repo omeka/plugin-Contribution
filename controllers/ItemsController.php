c2f91a4527a5949fd77d319568b25c7c124071e7
patrickmj <patrickmjchnm@gmail.com>
2015-02-22 11:59:44 -0500

    Strict Standards alignment

--- controllers/ItemsController.php
+++ controllers/ItemsController.php
@@ -3,7 +3,7 @@
 class Contribution_ItemsController extends Omeka_Controller_AbstractActionController
 {
     
-    public function _getBrowseRecordsPerPage()
+    public function _getBrowseRecordsPerPage($pluralName = null)
     {
         if (is_admin_theme()) {
             return (int) get_option('per_page_admin');
