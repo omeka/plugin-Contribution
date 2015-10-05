76ad31741256de82b4316490b0988a8a0d7a0611
patrickmj <patrickmjchnm@gmail.com>
2015-10-05 12:03:41 -0400

    handle some linky translations better

--- views/public/contribution/thankyou.php
+++ views/public/contribution/thankyou.php
@@ -1,10 +1,10 @@
 <?php echo head(); ?>
 <div id="primary">
 	<h1><?php echo __("Thank you for contributing!"); ?></h1>
-	<p><?php echo __("Your contribution will show up in the archive once an administrator approves it. Meanwhile, feel free to %s or %s ." , contribution_link_to_contribute(__('make another contribution')), "<a href='" . url('items/browse') . "'>" . __('browse the archive') . "</a>"); ?>
+	<p><?php echo __("Your contribution will show up in the archive once an administrator approves it. Meanwhile, feel free to <a href='" . contribution_contribute_url() . "'>%s</a> or <a href='" . url('items/browse') . "'>%s</a>." , __('make another contribution'), __('browse the archive') ); ?>
 	</p>
 	<?php if(get_option('contribution_simple') && !current_user()): ?>
-	<p><?php echo __("If you would like to interact with the site further, you can use an account that is ready for you. Visit %s, and request a new password for the email you used", "<a href='" . url('users/forgot-password') . "'>" . __('this page') . "</a>"); ?>
+	<p><?php echo __("If you would like to interact with the site further, you can use an account that is ready for you. Visit <a href='" . url('users/forgot-password') . "'>%s</a>, and request a new password for the email you used", __('this page')); ?>
 	<?php endif; ?>
 </div>
 <?php echo foot(); ?>
