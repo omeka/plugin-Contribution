69ae586262d6222a0203e0ca776085c558012dd1
patrickmj <patrickmjchnm@gmail.com>
2016-03-03 18:52:37 -0500

    add 'terms' to acl

--- ContributionPlugin.php
+++ ContributionPlugin.php
@@ -246,7 +246,7 @@ class ContributionPlugin extends Omeka_Plugin_AbstractPlugin
         $acl->addResource('Contribution_ContributorMetadata');
         $acl->addResource('Contribution_Types');
         $acl->addResource('Contribution_Settings');
-        $acl->allow(null, 'Contribution_Contribution', array('contribute', 'type-form', 'thankyou'));
+        $acl->allow(null, 'Contribution_Contribution', array('contribute', 'type-form', 'thankyou', 'terms'));
         $acl->addResource('Contribution_ContributorField');
         $acl->addResource('Contribution_ContributorValue');
         
