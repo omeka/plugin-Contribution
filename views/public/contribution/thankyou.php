d4431f8af2eac6418a2a55926b46bff30f3775da
Daniel Berthereau <Daniel.github@Berthereau.net>
2016-12-19 00:00:00 +0100

    Cleaned some public pages.

--- views/public/contribution/thankyou.php
+++ views/public/contribution/thankyou.php
@@ -1,4 +1,11 @@
-<?php echo head(); ?>
+<?php
+$title = __('Contribute');
+$bodyClass = 'contribution';
+
+echo head(array(
+    'title' => $title,
+    'bodyclass' => $bodyClass,
+)); ?>
 <div id="primary">
 	<h1><?php echo __("Thank you for contributing!"); ?></h1>
 	<p><?php echo __("Your contribution will show up in the archive once an administrator approves it. Meanwhile, feel free to %s or %s." , contribution_link_to_contribute(__('make another contribution')), "<a href='" . url('items/browse') . "'>" . __('browse the archive') . "</a>"); ?>
@@ -7,4 +14,4 @@
 	<p><?php echo __("If you would like to interact with the site further, you can use an account that is ready for you. Visit %s, and request a new password for the email you used.", "<a href='" . url('users/forgot-password') . "'>" . __('this page') . "</a>"); ?>
 	<?php endif; ?>
 </div>
-<?php echo foot(); ?>
+<?php echo foot();
