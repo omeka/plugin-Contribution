f3b9eb36d9e29489c28eff0d9257409ae98d4a70
patrickmj <patrickmjchnm@gmail.com>
2018-02-07 15:30:51 -0500

    more small tinymce cleanup

--- views/admin/javascripts/contribution.js
+++ views/admin/javascripts/contribution.js
@@ -1,8 +1,7 @@
 function setUpSettingsWysiwyg() {
     jQuery(window).load(function() {
         Omeka.wysiwyg({
-           mode: "specific_textareas",
-           editor_selector: "html-editor",
+           selector: ".html-editor",
            forced_root_block: ""
         });
     });
