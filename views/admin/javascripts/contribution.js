08f9f6b47b0a7579bd230cebd705467c91e0aef4
John Flatness <john@zerocrates.org>
2018-02-07 15:09:07 -0500

    Fix for new tinymce

--- views/admin/javascripts/contribution.js
+++ views/admin/javascripts/contribution.js
@@ -1,9 +1,8 @@
 function setUpSettingsWysiwyg() {
     jQuery(window).load(function() {
         Omeka.wysiwyg({
-           mode: "specific_textareas",
-           editor_selector: "html-editor",
-           forced_root_block: ""
+            selector: ".html-editor",
+            forced_root_block: ""
         });
     });
 }
