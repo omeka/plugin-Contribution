ba1273f03687a0832b5bdeafb31568c77b338a6c
Steven Barritt <mail@stevenbarritt.com>
2019-07-09 11:16:17 +0100

    Update AjaxController.php

--- controllers/AjaxController.php
+++ controllers/AjaxController.php
@@ -21,6 +21,48 @@ class Contribution_AjaxController extends Omeka_Controller_AbstractActionControl
 
     /**
      * Handle AJAX requests to update a record.
+     
+OLD:
+    public function updateAction()
+    {
+        if (!$this->_checkAjax('update')) {
+            return;
+        }
+
+        // Handle action.
+        try {
+            $status = $this->_getParam('status');
+            // The administrator can only make public/review a public
+            // contributed item: he cannot change the choice of the user here
+            // (public/private).
+            if (!in_array($status, array('proposed', 'approved', 'rejected'))) {
+                $this->getResponse()->setHttpResponseCode(400);
+                return;
+            }
+
+            $id = (integer) $this->_getParam('id');
+            $contributedItem = get_record_by_id('ContributionContributedItem', $id);
+            if (!$contributedItem) {
+                $this->getResponse()->setHttpResponseCode(400);
+                return;
+            }
+
+            // Check if the contributor set his item public or private.
+            if (!$contributedItem->public) {
+                // $this->getResponse()->setHttpResponseCode(400);
+                $this->getResponse()->setBody('private');
+                return;
+            }
+
+            // Update status (set public or to be reviewed).
+            // TODO Currently, only "Public" and "Needs review" status are managed.
+            $contributedItem->Item->public = ($status === 'approved') ? 1 : 0;
+            $contributedItem->Item->save();
+        } catch (Exception $e) {
+            $this->getResponse()->setHttpResponseCode(500);
+        }
+    }
+
      */
     public function updateAction()
     {
@@ -61,6 +103,8 @@ class Contribution_AjaxController extends Omeka_Controller_AbstractActionControl
             $this->getResponse()->setHttpResponseCode(500);
         }
     }
+
+
 
     /**
      * Handle AJAX requests to delete a record.
