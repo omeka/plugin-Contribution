c4c5e7455693e99a3650f6f200d1991549c2c79a
Kim Nguyen <design@kimisgold.com>
2021-04-08 10:40:08 -0400

    Use new quick filter pattern.

--- views/admin/common/contribution-quick-filters.php
+++ views/admin/common/contribution-quick-filters.php
@@ -1,11 +1,6 @@
-<ul class="quick-filter-wrapper">
-    <li><a href="#" tabindex="0"><?php echo __('Filter by status'); ?></a>
-    <ul class="dropdown">
-        <li><span class="quick-filter-heading"><?php echo __('Filter by status') ?></span></li>
-        <li><a href="<?php echo url('contribution/items'); ?>"><?php echo __('View All') ?></a></li>
-        <li><a href="<?php echo url('contribution/items', array('status' => 'public')); ?>"><?php echo __('Public'); ?></a></li>
-        <li><a href="<?php echo url('contribution/items', array('status' => 'private')); ?>"><?php echo __('Private'); ?></a></li>
-        <li><a href="<?php echo url('contribution/items', array('status' => 'review')); ?>"><?php echo __('Needs review'); ?></a></li>
-    </ul>
-    </li>
-</ul>
+<select class="quick-filter">
+    <option class="quick-filter-heading"><?php echo __('Filter by status') ?></option>
+    <option value="<?php echo url('contribution/items', array('status' => 'public')); ?>"><?php echo __('Public'); ?></option>
+    <option value="<?php echo url('contribution/items', array('status' => 'private')); ?>"><?php echo __('Private'); ?></option>
+    <option value="<?php echo url('contribution/items', array('status' => 'review')); ?>"><?php echo __('Needs review'); ?></option>
+</select>
