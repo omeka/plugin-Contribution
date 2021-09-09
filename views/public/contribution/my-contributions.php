bbc8b325bc844e351f93e66e89aa0ca0924d94bd
Kim Nguyen <design@kimisgold.com>
2021-09-09 15:46:16 -0400

    Add responsive table wrapper.

--- views/public/contribution/my-contributions.php
+++ views/public/contribution/my-contributions.php
@@ -8,32 +8,34 @@ echo head(array(
 <?php echo flash(); ?>
 <h1><?php echo $pageTitle;?> <?php echo __('(%s total)', $total_results); ?></h1>
 <?php if ($total_results): ?>
-<form method='post'>
-    <table>
-        <thead>
-            <tr>
-                <th><?php echo __('Public'); ?></th>
-                <th><?php echo __('Anonymous'); ?></th>
-                <th><?php echo __('Item'); ?></th>
-                <th><?php echo __('Added'); ?></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach(loop('contrib_items') as $contribItem): ?>
-            <?php $item = $contribItem->Item; ?>
-            <tr>
-                <td><?php echo $this->formCheckbox("contribution_public[{$contribItem->id}]", null, array('checked'=>$contribItem->public) ); ?>
-                </td>
-                <td><?php echo $this->formCheckbox("contribution_anonymous[{$contribItem->id}]", null, array('checked'=>$contribItem->anonymous) ); ?>
-                </td>                
-                <td><?php echo link_to($item, 'show', metadata($item, array('Dublin Core', 'Title'))); ?></td>
-                <td><?php echo metadata($item, 'added'); ?></td>
-            
-            </tr>
-            
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+    <form method='post'>
+    <div class="table-responsive">
+        <table>
+            <thead>
+                <tr>
+                    <th><?php echo __('Public'); ?></th>
+                    <th><?php echo __('Anonymous'); ?></th>
+                    <th><?php echo __('Item'); ?></th>
+                    <th><?php echo __('Added'); ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach(loop('contrib_items') as $contribItem): ?>
+                <?php $item = $contribItem->Item; ?>
+                <tr>
+                    <td><?php echo $this->formCheckbox("contribution_public[{$contribItem->id}]", null, array('checked'=>$contribItem->public) ); ?>
+                    </td>
+                    <td><?php echo $this->formCheckbox("contribution_anonymous[{$contribItem->id}]", null, array('checked'=>$contribItem->anonymous) ); ?>
+                    </td>                
+                    <td><?php echo link_to($item, 'show', metadata($item, array('Dublin Core', 'Title'))); ?></td>
+                    <td><?php echo metadata($item, 'added'); ?></td>
+                
+                </tr>
+                
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
     <input id="save-changes" class="submit big button" type="submit" value="Save Changes" name="submit">
 </form>
 <?php else: ?>
