4017bf6fbca30f38f55570afa89569c132bfd028
Kim Nguyen <design@kimisgold.com>
2022-07-06 16:38:18 -0400

    Remove periods following links ending strings. #74

--- views/admin/index/index.php
+++ views/admin/index/index.php
@@ -22,20 +22,20 @@ echo $this->partial('contribution-navigation.php');
         <dt>1. Modify the contribution form:</dt>
         <dd>
             <ul>
-                <li>Choose item types you wish visitors to share in <a href="<?php echo url('contribution/types'); ?>">Contribution Types</a>.</li>
-                <li>Pick the fields you want users to complete by editing each item type in <a href="<?php echo url('contribution/types'); ?>">Contribution Types</a>.</li>
-                <li>Create questions about contributors in <a href="<?php echo url('contribution/contributor-metadata'); ?>">Contributor Questions</a>.</li>
+                <li>Choose item types you wish visitors to share in <a href="<?php echo url('contribution/types'); ?>">Contribution Types</a></li>
+                <li>Pick the fields you want users to complete by editing each item type in <a href="<?php echo url('contribution/types'); ?>">Contribution Types</a></li>
+                <li>Create questions about contributors in <a href="<?php echo url('contribution/contributor-metadata'); ?>">Contributor Questions</a></li>
             </ul>
         </dd>
         <dt>2. Configure settings for submitting contributions:</dt>
         <dd>
             <ul>
-                <li>Set the terms of service for contributing to the site in <a href="<?php echo url('contribution/settings'); ?>">Submission Settings</a>.</li>
-                <li>Set up an auto-generated email to send to all contributors after they submit their contribution in <a href="<?php echo url('contribution/settings'); ?>">Submission Settings</a>.</li>
-                <li>Specify a collection for new contributed items in <a href="<?php echo url('contribution/settings'); ?>">Submission Settings</a>.</li>
+                <li>Set the terms of service for contributing to the site in <a href="<?php echo url('contribution/settings'); ?>">Submission Settings</a></li>
+                <li>Set up an auto-generated email to send to all contributors after they submit their contribution in <a href="<?php echo url('contribution/settings'); ?>">Submission Settings</a></li>
+                <li>Specify a collection for new contributed items in <a href="<?php echo url('contribution/settings'); ?>">Submission Settings</a></li>
             </ul>
         </dd>
-        <dt>3. Browse contributors' names, emails, and items in <a href="<?php echo url('contribution/contributors'); ?>">Contributors</a>.</dt>
+        <dt>3. Browse contributors' names, emails, and items in <a href="<?php echo url('contribution/contributors'); ?>">Contributors</a></dt>
     </dl>
     </div>
 </div>
